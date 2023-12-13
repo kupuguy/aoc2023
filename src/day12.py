acd3ed282589cd3c9af6b7d29d9b886c26cd50e6
Duncan Booth <kupuguy@gmail.com>
2023-12-13 07:21:52 +0000

    Day 13

---
+++ src/day12.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+from typing import Sequence
+from functools import cache
+
+input = Path("input/day12.txt").read_text().splitlines()
+
+
+TEST_DATA = """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1""".splitlines()
+EXPECTED = 21
+
+
+def parse(input: list[str]) -> Sequence[tuple[str, list[int]]]:
+    for line in input:
+        left, right = line.strip().split()
+        yield left, [int(n) for n in right.split(",")]
+
+
+@cache
+def matches(mask: str, values: tuple[int]) -> int:
+    # print(mask, values)
+    if not values:
+        return 0 if "#" in mask else 1
+
+    current = values[0]
+    rest = values[1:]
+    if current == len(mask) and "." not in mask and not rest:
+        return 1
+
+    rightmost = len(mask) - (sum(rest) + (len(rest) - 1)) - current
+    total = 0
+    # print(f"{rightmost=}, {rest=}")
+    for pos in range(0, rightmost):
+        left, masked, sep, right = (
+            mask[:pos],
+            mask[pos : pos + current],
+            mask[pos + current : pos + current + 1],
+            mask[pos + current + 1 :],
+        )
+        if "#" in left:
+            # no more matches possible
+            return total
+        if "." not in masked and sep in (".", "?", ""):
+            n = matches(right, rest)
+            # print(f"{left}[{masked}]{sep}{right} -> {n}")
+            total += n
+    return total
+
+
+assert matches("?###????????", (3, 2, 1)) == 10
+assert matches("???.###", (1, 1, 3)) == 1
+assert matches(".??..??...?##.", (1, 1, 3)) == 4
+
+
+def part1(input: list[str], unfold: bool = False) -> int:
+    total = 0
+    for mask, values in parse(input):
+        if unfold:
+            mask = "?".join([mask] * 5)
+            values = values * 5
+        n = matches(mask, tuple(values))
+        # print(f"{n}: {mask} {values}")
+        total += n
+    return total
+
+
+res = part1(TEST_DATA)
+assert res == EXPECTED
+
+print(part1(input))
+
+assert part1(TEST_DATA, unfold=True) == 525152
+print(part1(input, unfold=True))  # 204640299929836
---
+++ src/day13.py
@@ -0,0 +1,105 @@
+from pathlib import Path
+from typing import Sequence
+from functools import cache
+
+input = Path("input/day13.txt").read_text().splitlines()
+
+TEST_DATA = """#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#""".splitlines()
+
+EXPECTED = 405
+
+
+def blocks(input: list[str]) -> Sequence[list[str]]:
+    block: list[str] = []
+    for line in input:
+        if line:
+            block.append(line)
+        else:
+            if block:
+                yield block
+            block = []
+    if block:
+        yield block
+
+
+def transpose(lines: list[str]) -> list[str]:
+    return ["".join(s) for s in zip(*lines)]
+
+
+def reflections(block: list[str]) -> Sequence[int]:
+    for index in range(1, len(block)):
+        nrange = min(index, len(block) - index)
+        # print(len(block), index, index - (nrange-1) - 1, index + nrange-1)
+        if all(
+            block[index - n - 1] == block[index + n]
+            for n in range(nrange)
+        ):
+            yield index
+
+
+def part1(input: list[str]) -> int:
+    total = 0
+    for block in blocks(input):
+        # print(*block, sep="\n")
+        total += sum(reflections(block)) * 100
+
+        transposed = transpose(block)
+        # print(*transpose(block), sep="\n")
+        total += sum(reflections(transposed))
+
+    return total
+
+
+res = part1(TEST_DATA)
+assert res == EXPECTED
+
+print(part1(input))
+# > 20700
+
+def diff_by_one(line1: str, line2: str) -> bool:
+    return sum(1 if a != b else 0 for a,b in zip(line1, line2)) == 1
+
+def smudged(block: list[str]) -> int:
+    for index in range(1, len(block)):
+        nrange = min(index, len(block) - index)
+        # print(len(block), index, index - (nrange-1) - 1, index + nrange-1)
+        seen_diff = False
+        match = True
+        for n in range(nrange):
+            if block[index - n - 1] != block[index + n]:
+                if not seen_diff and diff_by_one(block[index - n - 1], block[index + n]):
+                    seen_diff = True
+                    continue
+                match = False
+                break
+        if match and seen_diff:
+            return index
+    return 0
+
+def part2(input: list[str]) -> int:
+    total = 0
+    for block in blocks(input):
+        total += smudged(block) * 100
+
+        transposed = transpose(block)
+        # print(*transpose(block), sep="\n")
+        total += smudged(transposed)
+
+    return total
+
+assert part2(TEST_DATA) == 400
+print(part2(input))
